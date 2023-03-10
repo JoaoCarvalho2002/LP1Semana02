c48bac8620f730818399c22c70e475d9eaf7c42b
JoaoCarvalho2002 <jonitkick@gmail.com>
2023-03-10 10:48:30 +0000

    exercicio 1,ainda não acabado

---
+++ ManipulaVar/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ManipulaVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n1=Console.ReadLine();
+            
+            int n2=Convert.ToInt32(n1);
+            Console.WriteLine(n1);
+        }
+    }
+}
