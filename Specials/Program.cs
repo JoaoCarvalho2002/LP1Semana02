772684457e7254c70decdf824d7ffec63697a2b1
JoaoCarvalho2002 <jonitkick@gmail.com>
2023-03-10 11:59:51 +0000

    Exercicio 6 completo

---
+++ Specials/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Specials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            int xI = int.MaxValue;
+            int xi = int.MinValue;
+            
+            short xSH = short.MaxValue;
+            short xsh = short.MinValue;
+            
+            sbyte xSB = sbyte.MaxValue;
+            sbyte xsb = sbyte.MinValue;
+            
+            decimal xDC = decimal.MaxValue;
+            decimal xdc = decimal.MinValue;
+
+            long xL = long.MaxValue;
+            long xl = long.MinValue;
+
+            float xf = float. PositiveInfinity;
+            double xd = double. PositiveInfinity;
+
+            Console.WriteLine(xI);
+            Console.WriteLine(xi);
+            Console.WriteLine(xSH);
+            Console.WriteLine(xsh);
+            Console.WriteLine(xSB);
+            Console.WriteLine(xsb);
+            Console.WriteLine(xDC);
+            Console.WriteLine(xdc);
+            Console.WriteLine(xL);
+            Console.WriteLine(xl);
+            Console.WriteLine(xf);
+            Console.WriteLine(xd);
+
+        }
+    }
+}
