1e48c7acbe7c179e73ee7e926349a70f0dbe421b
JoaoCarvalho2002 <jonitkick@gmail.com>
2023-03-10 16:07:31 +0000

    Exercicio 9 completo

---
+++ ChatGPTMother/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ChatGPTMother
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string quest, resp;
+            Boolean EXIT=false;
+            while(EXIT==false){
+
+                Console.Write("Place your question? ");
+                quest = Console.ReadLine();
+            if (quest=="Exit" || quest =="exit"){
+                EXIT=true;
+            }
+                resp = quest switch
+            {
+                "How are you?" => "I'm fine, thank you",
+
+                "What's your name?" =>"Mother",
+
+                "1+1?" =>"2, two plus two is four,Minus one thats three,Quick Maths",
+
+                "Who is the best programmer?"=>"You!!",
+                     
+                "Exit" =>"Shuting-down",
+
+                "exit" =>"Shuting-down",
+                    
+                _ => "Não reconheço esta frase",
+                     
+                  
+            };
+            Console.WriteLine(resp);
+
+            }
+
+            
+        }
+    }
+}
