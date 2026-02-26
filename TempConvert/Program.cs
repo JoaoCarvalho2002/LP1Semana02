42cbe6b7fef4fdcbca9ad6ae36682ad037fc7646
JoaoCarvalho2002 <jonitkick@gmail.com>
2026-02-26 16:11:12 +0000

    Pede temperatura e unidade

---
+++ TempConvert/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TempConvert
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string temp="";
+            string unit="";
+            //Console.WriteLine("Hello LP!");
+            Console.WriteLine("Temperature?");
+            temp=Console.ReadLine();
+            Console.WriteLine("C or F?");
+            unit=Console.ReadLine();
+            Console.WriteLine(temp+unit);
+        }
+    }
+}
